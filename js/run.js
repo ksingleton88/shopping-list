f0689020461acfb7f8eac72e40960333ab465cac
ksingleton88 <ksingleton88@gmail.com>
2016-04-14 10:59:28 -0700

    Initial Commit

--- js/run.js
+++ js/run.js
@@ -4,7 +4,41 @@ $(document).ready(function () {
 		$(".list").append("<li class='item'>" + " " + toAdd + "</li>");
 	});
 
-	$(document).on('click', '.list-items', function() {
+
+
+	// $('li').hover(function(){
+	// 	$(this).append($('<span> X </span>') );
+	// }, function(){
+	// 	$(this).find('span:last').remove();
+	// 	}
+	// );
+
+     $(document).on('mouseenter', 'li', function(){
+		$(this).append($('<span> X </span>') );
+	}).on('mouseleave', 'li', function(){
+		$(this).find('span:last').remove();
+        });
+
+
+	$(document).on('click', 'li', function() {
 		$(this).remove();
 	});
+
+
+
+
+	$(document).on('keydown', function(e) {
+		var code = e.keyCode;
+		var toAdd =$("input[name=checkListItem]").val();
+			if(code == 13){
+				event.preventDefault();
+				$(".list").append("<li class='item'>" + " " + toAdd + "</li>");
+				return false;
+			};
+
+	});
 });
+
+
+
+//when hovering over text, 'x' image does not show on new text elements. 
